ed97b9b5c212cd49744fa14b05f57d154d99e651
Gabriel Avrahami <117397869+Gabriel-Avrahami@users.noreply.github.com>
2025-01-08 23:10:06 -0500

    Update client.c with the most recent version of basic_client.c

--- client.c
+++ client.c
@@ -1,17 +1,31 @@
 #include "pipe_networking.h"
 
-int main() {
-  int to_server;
-  int from_server;
-
-  from_server = client_handshake( &to_server );
-
-  char str[16];
-  while (read(from_server, str, 16)) {
-    printf("%s\n", str);
-  }
+static void sighandler(int signo) {
   int pid = getpid();
   char pp[16];
   sprintf(pp, "%d", pid);
   remove(pp);
+  exit(0);
+}
+
+int main() {
+  signal(SIGPIPE, sighandler);
+  signal(SIGINT, sighandler);
+  int to_server;
+  int from_server;
+
+  from_server = client_handshake( &to_server );
+  
+  while (1) {
+    char pid[32] = "my pid is ";
+    char id[16] = "";
+    sprintf(id, "%d", getpid());
+    strcat(pid, id);
+    write(to_server, pid, strlen(pid));
+    
+    char str[16];
+    read(from_server, str, 16);
+    printf("Returned: %s\n", str);
+    sleep(1);
+  }
 }
