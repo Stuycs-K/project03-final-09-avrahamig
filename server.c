edd42942e033ecbf1a87967049f9c566b005c84e
Gabriel Avrahami <gavrahami50@stuy.edu>
2025-01-16 14:38:39 -0500

    o m gs it's so close. The beginning works now. The parent prints slightly confusing stuff, but nows I gotta fix the end. I could run it one more time, or try to remove the bad stuff to make sure I actually get it

--- server.c
+++ server.c
@@ -29,7 +29,7 @@ int main(int argc, char * argv[]) {
   signal(SIGPIPE, sighandler);
 
   int numPlayers = atoi(argv[1]);
-  int numRounds = numPlayers - 1;
+  int numRounds = numPlayers;
   if (argc == 3) {
     numRounds = atoi(argv[2]);
   }
